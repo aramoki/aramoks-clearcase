53365a8d7158f920068f4c67f00aa2b293b7dc05
aramok <mohorame@gmail.com>
2019-10-12 14:52:45 +0300

    2

--- src/cleartool.ts
+++ src/cleartool.ts
@@ -2,16 +2,14 @@
 import { exec, spawn, ChildProcess, ExecException } from 'child_process';
 
 export class ClearTool {
-    greeting: string;
-
-
+    tool: string;
 
     constructor() {
-        this.greeting = "message";
+        this.tool = "cleartool";
     }
  
     run_command(command:String , path:String ,c_error: (exception: ExecException, val: String) => void ,std_error: (val : string) => void , std_success: (val : string) => void){
-        exec("cleartool "+ command + " " + path + "", (error, stdout, stderr) => {
+        exec(this.tool + " "+ command + " " + path + "", (error, stdout, stderr) => {
             if (error) {
                 c_error(error,stderr);
                 return;
